c3bbc56fb45bbab260e7d2ae1edc0e3c43d1dd57
santibraby <santibraby.space@gmail.com>
2025-06-16 17:38:07 -0400

    simplify 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ st.markdown("""
         position: relative;
         opacity: 1;
         transition: all 0.3s ease;
-        border: 1px solid #e0e0e0;
+        border: 0px solid #e0e0e0;
     }
 
     .circle.loaded {
